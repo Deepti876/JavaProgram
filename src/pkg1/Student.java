e6a580197e10b19cb5b3967d7c747304783880d3
Deepti Yadav <deeptiyadav1206@gmail.com>
2021-11-27 17:39:47 +0530

    first commit

---
+++ src/pkg1/Student.java
@@ -0,0 +1,15 @@
+package pkg1;
+
+public class Student {
+        int a;
+		public void abc()
+		{
+			System.out.print("Hello World");
+		}
+		public static void main(String[] args) 
+		{ 
+			Student x=new Student();
+			x.abc();
+	    }
+
+}
